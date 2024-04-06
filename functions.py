3fe7895812b9ada22d6d3fe176779b2d42e6b33b
Buzip <164242635+DCL-P@users.noreply.github.com>
2024-04-06 22:17:51 +0200

    returned list is now a str
    also made a few changes to the prints

--- functions.py
+++ functions.py
@@ -1,14 +1,13 @@
 import random
-import string
 
 def generate_password(number_of_characters: int) -> list:
     password = []
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's']
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     
     number_of_characters = int(number_of_characters)
     for characters in range(number_of_characters):
         password.append(random.choice(alphabet))
     
-    print(number_of_characters)
-    return password
+    generated_password = ''.join(password)
+    return f'enjoy your new password!:  \033[93m{generated_password}\033[0m'
         
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from random import *
 from functions import *
 
-number_of_characters = input('select the amount of characters in your password:  ')
-
-try:
-    int(number_of_characters)
-    print(generate_password(number_of_characters))
-except ValueError:
-    print('NOPE')
+while True:
+    number_of_characters = input('select the amount of characters in your password:  ')
+    try:
+        int(number_of_characters)
+        print(generate_password(number_of_characters))
+        break
+    except ValueError:
+        print()
+        print('\033[91mu can only enter numbers!\033[0m')
+        print()
+        continue
 
