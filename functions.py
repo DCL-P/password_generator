d8023cec3b5abdb638902beb7698bfb20720b39b
Buzip <164242635+DCL-P@users.noreply.github.com>
2024-04-07 12:36:11 +0200

    added more characters to the characters list

--- functions.py
+++ functions.py
@@ -2,7 +2,7 @@ import random
 
 def generate_password(number_of_characters: int) -> list:
     password = []
-    characters_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    characters_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '?', '!', '@', '#', '$', '%', '^', '&', '*']
     
     number_of_characters = int(number_of_characters)
     for characters in range(number_of_characters):
